7265e3e909c825c837735af2e1da23d6230af42e
mayanklbg <115223987+mayanklbg@users.noreply.github.com>
2022-11-29 22:12:17 +0000

    feat CPE-001, Update docker file (#15)
    * feat CPE-001, Update docker file
    
    * feat CPE-001, Update docker file
    
    Co-authored-by: mayanklbg <mayank.agarwal@lloydsbanking.com>

---
+++ main.tf
@@ -0,0 +1,16 @@
+module {
+  source             = "app.terraform.io/lbg-cloud-platform/gcp-project/google"
+  version            = "4.0.0"
+  project_name       = var.project_kube_02_svc_name
+  billing_account_id = var.billing_account_id
+  parent_folder      = var.parent_folder
+  project_id         = var.project_kube_02_svc_name
+}
+module {
+  source             = "app.terraform.io/lbg-cloud-platform/gcp-project/google"
+  version            = "4.0.0"
+  project_name       = var.project_atch_02_svc_name
+  billing_account_id = var.billing_account_id
+  parent_folder      = var.parent_folder
+  project_id         = var.project_atch_02_svc_name
+}
