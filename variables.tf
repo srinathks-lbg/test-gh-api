cae7d57701e8f4c212b49faa294134c158edfeeb
mayanklbg <115223987+mayanklbg@users.noreply.github.com>
2022-11-29 22:13:13 +0000

    feat CPE-001, Update docker file (#16)
    * feat CPE-001, Update docker file
    
    * feat CPE-001, Update docker file
    
    Co-authored-by: mayanklbg <mayank.agarwal@lloydsbanking.com>

--- variables.tf
+++ variables.tf
@@ -6,3 +6,11 @@ variable "project_atch_02_svc_name" {
   type    = string
   default = "pdev-mngd-02-atch-02"
 }
+variable "project_kube_02_svc_name" {
+  type    = string
+  default = "pdev-mngd-02-kube-02"
+}
+variable "project_atch_02_svc_name" {
+  type    = string
+  default = "pdev-mngd-02-atch-02"
+}
