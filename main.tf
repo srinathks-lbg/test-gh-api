e4c00b3af17471133ea9ff8fa748933d355ace51
mayanklbg <mayank.agarwal@lloydsbanking.com>
2022-11-29 23:58:06 +0000

    feat CPE-001, Update docker file

--- main.tf
+++ main.tf
@@ -1,16 +1,16 @@
 module "create-kube-03-svc-project" {
-  source             = "app.terraform.io/lbg-cloud-platform/gcp-project/google"
-  version            = "4.0.0"
-  project_name       = "var.project_kube_03_svc_name"
+  source  = "app.terraform.io/lbg-cloud-platform/gcp-project/google"
+  version = "4.0.0"
+  project_name       = var.project_kube_03_svc_name"
   billing_account_id = var.billing_account_id
   parent_folder      = var.parent_folder
-  project_id         = "var.project_kube_03_svc_name"
+  project_id         = "var.project_kube_03_svc_name
 }
 module "create-atch-03-svc-project" {
-  source             = "app.terraform.io/lbg-cloud-platform/gcp-project/google"
-  version            = "4.0.0"
-  project_name       = "var.project_atch_03_svc_name"
+  source  = "app.terraform.io/lbg-cloud-platform/gcp-project/google"
+  version = "4.0.0"
+  project_name       = var.project_atch_03_svc_name"
   billing_account_id = var.billing_account_id
   parent_folder      = var.parent_folder
-  project_id         = "var.project_atch_03_svc_name"
+  project_id         = "var.project_atch_03_svc_name
 }
